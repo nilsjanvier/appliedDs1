f1e55a7bbcb8e9469909cbda2b6ce572923c5f0d
nilsjanvier <60745167+nilsjanvier@users.noreply.github.com>
2020-03-17 08:44:38 +0100

    initial commit

---
+++ appliedDs1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 08:39:14 2020
+
+@author: Admin
+"""
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv("base_earnings.csv")
+
+
+
+plt.plot(df.GDP)
+
+plt.show()
